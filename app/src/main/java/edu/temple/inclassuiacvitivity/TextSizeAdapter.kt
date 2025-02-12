598c9b592577dc5d7d0f9c0cb9a563fb688524d7
HenryW7175 <143651504+HenryWu7175@users.noreply.github.com>
2025-02-11 23:37:06 -0500

    added dropdown spinner view with funcitonality. the selections madein the view can changethe size of the text view on display

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -1,25 +1,44 @@
 package edu.temple.inclassuiacvitivity
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
-class TextSizeAdapter : BaseAdapter() {
+class TextSizeAdapter(_context : Context, _fonts : Array<Int>) : BaseAdapter() {
+
+    private val context = _context
+    private val fonts = _fonts
+
 
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        return fonts.size
     }
 
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+        return fonts[position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+        return position.toLong()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+        val textView = TextView(context)
+////        textView.text = fonts[position].toString()
+//        textView.setPadding(5,10,0,10)
+//        if(convertView  == null     //this implemetns the
+            textView.text = fonts[position].toString()
+            return textView
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val textView = (getView(position, convertView, parent) as TextView)
+        textView.text = fonts[position].toString()
+        textView.textSize = fonts[position].toFloat()
+        return textView
+//        return super.getDropDownView(position, convertView, parent).apply{}
     }
 
 }
